396c8d2670b60de9e9b14fc084c03d68a2b7ff3d
Sayantan Mandal <sayantanmandal174@gmail.com>
2023-04-16 10:55:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-function threeSum(arr, target) {
+function threeSum(num) {
 // write your code here
-  
+  let b = 0;
+  let i = 1;
+  while (a != 0) {
+    let rem = a % 2;
+    b = b + rem * i;
+    i = i * 10;
+    a = Math.floor(a / 2);
+  }
+  return b;
 }
 
 module.exports = threeSum;
