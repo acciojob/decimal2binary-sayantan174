66e9730543b8b43c3cb81e97366d3a1d66800ef8
Sayantan Mandal <sayantanmandal174@gmail.com>
2023-04-29 20:04:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,10 @@
-function threeSum(nums, target) {
-  nums.sort((a, b) => a - b); // Sort the array in ascending order
-  let closestSum = nums[0] + nums[1] + nums[2]; // Initialize the closest sum to the sum of the first three numbers
-  for (let i = 0; i < nums.length - 2; i++) {
-    let left = i + 1;
-    let right = nums.length - 1;
-    while (left < right) {
-      const sum = nums[i] + nums[left] + nums[right];
-      if (Math.abs(target - sum) < Math.abs(target - closestSum)) {
-        closestSum = sum; // Update closest sum if the current sum is closer to target
-      }
-      if (sum < target) {
-        left++; // If the current sum is less than target, move the left pointer to increase the sum
-      } else if (sum > target) {
-        right--; // If the current sum is greater than target, move the right pointer to decrease the sum
-      } else {
-        return sum; // If the current sum equals target, return it
-      }
-    }
+function threeSum(decimal, target) {
+	let binary = '';
+  while (decimal > 0) {
+    binary = (decimal % 2) + binary;
+    decimal = Math.floor(decimal / 2);
   }
-  return closestSum; // Return the closest sum
+  return binary || '0'; // Return '0' for input of 0
 }
 
 module.exports = threeSum;
